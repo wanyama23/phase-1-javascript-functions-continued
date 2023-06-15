c86d33f20b65e2b399e76e0c6ba44739fb1a62b7
edmond <edmond.wanyama@student.moringaschool.com>
2023-06-15 09:46:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string="*") {
+    return function(parameter="special") {
+        return `You are ${string}${parameter}${string}!`
+    }
+}
+
+let Calculator = {
+    add: function(num1, num2) {
+        return num1 + num2
+    },
+    subtract: function(num1, num2) {
+        return num1 - num2
+    },
+    multiply: function(num1, num2) {
+        return num1 * num2
+    },
+    divide: function(num1, num2) {
+        return num1 / num2
+    }
+}
+
+function actionApplyer(startingInteger, arrayOfFunctions) {
+    let num = startingInteger
+
+    for (let i = 0; i < arrayOfFunctions.length; i++) {
+        num = arrayOfFunctions[i](num)
+        }
+    return num
+}
